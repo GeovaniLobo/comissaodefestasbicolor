65d6bb863e744a78bec282233b92ceb2b8d0f09e
GeovaniLobo <geovanilobo98@gmail.com>
2024-03-10 19:42:27 -0300

    [primeiro]

---
+++ entrar/script.js
@@ -0,0 +1,12 @@
+const text = "Seja Bem-Vindo ao Nosso Site.";
+let index = 0;
+
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 50); // Altere esse valor para ajustar a velocidade da digitação
+  }
+}
+
+typeWriter();
